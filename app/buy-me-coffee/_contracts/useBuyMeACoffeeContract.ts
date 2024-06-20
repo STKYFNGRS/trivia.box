2ff846bd585d8e57b28c5d987413e2acbb62701e
Alex M <contact@stkyfngrs.com>
2024-06-19 21:35:59 -0700

    commit

--- app/buy-me-coffee/_contracts/useBuyMeACoffeeContract.ts
+++ app/buy-me-coffee/_contracts/useBuyMeACoffeeContract.ts
@@ -9,7 +9,7 @@ export const useBuyMeACoffeeContract = generateContractHook({
   abi: BuyMeACoffeeABI,
   [baseSepolia.id]: {
     chain: baseSepolia,
-    address: '0x2001dce233105E4F8b32D4AC9C92203F9B4052f8',
+    address: '0x2001dce233105e4f8b32d4ac9c92203f9b4052f8',
   },
 
   // ... more chains for this contract go here
