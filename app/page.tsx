414e08d20a8b54ee09b9e3decfc6f415b6616eec
Alex M <contact@stkyfngrs.com>
2024-06-25 21:08:48 -0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { generateMetadata } from '@/utils/generateMetadata';
 import HomePage from './home/HomePage';
 
 export const metadata = generateMetadata({
-  title: 'Build Onchain Apps Template',
+  title: 'Trivia, Games and Puzzles',
   description:
-    'Save weeks of initial app setup and the hassle of integrating onchain components with web2 infrastructure.',
+    'Earn rewards, win prizes, join the community.',
   images: 'themes.png',
   pathname: '',
 });
