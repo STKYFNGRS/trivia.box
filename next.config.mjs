f97cea97c580150e6000d2c455d7017b296997ac
Alex M <contact@stkyfngrs.com>
2025-01-31 12:28:27 -0800

    commit

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-export default {
-  webpack: (config) => {
-    config.externals = config.externals || [];
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
-  },
-};
